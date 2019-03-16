1ef1541e822c505a982137135ae50cad9d58fd9a
Kevin Harvell <kevharvell@gmail.com>
2019-03-15 23:09:25 -0500

    Experimenting with polymorphism and OOP

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,17 +8,16 @@ using System.Threading.Tasks;
 
 namespace Grades
 {
-    public class GradeBook
+    public class GradeBook : GradeTracker
     {
-        private List<float> grades;
-        private string _name;
-        public event NameChangedDelegate NameChanged;
+        protected List<float> grades;
+        
         public GradeBook()
         {
             grades = new List<float>();
         }
 
-        public void WriteGrades(TextWriter destination)
+        public override void WriteGrades(TextWriter destination)
         {
             for (int i = 0; i < grades.Count; i++)
             {
@@ -25,38 +25,12 @@ namespace Grades
             }
         }
 
-        public string Name
-        {
-            get
-            {
-                return _name;
-            }
-            set
-            {
-                if (String.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentException("Name cannot be null or empty");
-                }
-
-                if (_name != value)
-                {
-                    NameChangedEventArgs args = new NameChangedEventArgs();
-                    args.ExistingName = _name;
-                    args.NewName = value;
-
-                    NameChanged(this, args);
-                }
-
-                _name = value;
-            }
-        }
-
-        public void AddGrade(float grade)
+        public override void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
-        public GradeStatistics ComputeStatistics()
+        public override GradeStatistics ComputeStatistics()
         {
             GradeStatistics stats = new GradeStatistics();
 
@@ -70,6 +44,11 @@ namespace Grades
 
             stats.AverageGrade = sum / grades.Count;
             return stats;
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return grades.GetEnumerator();
         }
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -11,23 +11,35 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-            GradeBook book = new GradeBook();
+            IGradeTracker book = NewMethod();
             GetBookName(book);
             AddGrades(book);
-            NewMethod(book);
+            WriteGrades(book);
             WriteResults(book);
         }
 
-        private static void WriteResults(GradeBook book)
+        private static GradeTracker NewMethod()
         {
+            return new ThrowAwayGradeBook();
+        }
+
+        private static void WriteResults(IGradeTracker book)
+        {
+
             GradeStatistics stats = book.ComputeStatistics();
+
+            foreach(float grade in book)
+            {
+                Console.WriteLine(grade);
+            }
+
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", (int)stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
             WriteResult(stats.Description, stats.LetterGrade);
         }
 
-        private static void NewMethod(GradeBook book)
+        private static void WriteGrades(IGradeTracker book)
         {
             using (StreamWriter outputFile = File.CreateText("grades.txt"))
             {
@@ -35,14 +47,14 @@ namespace Grades
             }
         }
 
-        private static void AddGrades(GradeBook book)
+        private static void AddGrades(IGradeTracker book)
         {
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
         }
 
-        private static void GetBookName(GradeBook book)
+        private static void GetBookName(IGradeTracker book)
         {
             try
             {
